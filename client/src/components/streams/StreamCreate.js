77e446e7204cf3bcba85c3e2052b98bf4c3be92b
jakehurrell <hakejurrell@gmail.com>
2019-06-16 21:34:07 -0400

    finished api db communication. End 243

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import {createStream} from '../../actions';
 
 class StreamCreate extends React.Component {
 
@@ -26,8 +28,8 @@ class StreamCreate extends React.Component {
     );
   }
 
-  onSubmit(formValues) {
-    console.log(formValues);
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
   }
 
   render() {
@@ -55,7 +57,16 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate: validate
 })(StreamCreate);
+
+export default connect(null, {createStream})(formWrapped);
+
+//works too
+// export default connect()(reduxForm({
+//   form: 'streamCreate',
+//   validate: validate
+// })(StreamCreate));
